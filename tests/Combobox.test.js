37af735a5c7e0804e50c56abaeff6b8bd8731e96
Marouen Mhiri <marujah@gmail.com>
2018-11-27 15:31:14 +0100

    refactor: fix typos

--- tests/Combobox.test.js
+++ tests/Combobox.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Combobox from '../lib/components/Combobox';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('Combobox Component', () => {
-    test('renders', () => {
+    test('Combobox renders', () => {
         const box = shallow(<Combobox data={[]} />);
         expect(box.exists()).toBe(true);
+    });
+    test('renders StyledComponent', () => {
+        const wrapper = mount(<Combobox data={[]} />);
+        expect(wrapper.find('StyledComponent')).toHaveLength(1);
     });
 });
