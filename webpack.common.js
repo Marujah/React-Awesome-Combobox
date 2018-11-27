ba8d80279f8295f294a4a7d418a17afb5a52edf9
Marouen Mhiri <marujah@gmail.com>
2018-11-27 21:58:46 +0100

    fix babel versions

--- webpack.common.js
+++ webpack.common.js
@@ -18,9 +18,6 @@ module.exports = {
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
-                    options: {
-                        presets: ["babel-preset-env", "babel-preset-es2015", "babel-preset-react"]
-                    }
                 }
             }
         ]
