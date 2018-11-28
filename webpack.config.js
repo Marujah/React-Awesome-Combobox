6ff07097733ac92eb572082349b1cd6ff934fdbc
Marouen Mhiri <marujah@gmail.com>
2018-11-28 10:35:57 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
 var path = require('path');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-    entry: './lib/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'index.js',
         libraryTarget: 'commonjs2'
     },
+    mode: "production",
+    optimization: {
+        // minimize default is true
+        minimizer: [
+            // Optimize/minimize CSS assets.
+            // Solves extract-text-webpack-plugin CSS duplication problem
+            // By default it uses cssnano but a custom CSS processor can be specified
+            new UglifyJsPlugin(),
+        ]
+    },
     module: {
         rules: [
             {
                 test: /\.js$/,
-                include: path.resolve(__dirname, 'lib'),
-                exclude: /(node_modules|build)/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|dist)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['env']
                     }
                 }
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i, 
-                loader: "file-loader?name=/src/assets/images/[name].[ext]"
             },
             {
                 test: /\.css$/,
@@ -38,6 +46,9 @@ module.exports = {
             }
         ]
     },
+    plugins: [
+        new CleanWebpackPlugin(["build"]),
+    ]
     // externals: {
     //     'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
     // }
